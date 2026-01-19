550404e2f59665c70f44ef91246e2cccafe96bcd
Ujjwal Kar <ujjwalkar21@gmail.com>
2026-01-19 23:25:10 +0530

    fix: Set default values for SECRET_KEY and ALLOWED_HOSTS in settings.py for improved security

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "2kx3c54jyol&o%u0rpg@pbm4mivmncjxqk#-hmx)v#u2sa109!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 AUTH_USER_MODEL = "accounts.User"
 
